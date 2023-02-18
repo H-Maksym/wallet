d1e7ec540558de6ba736ec39051f17397eb67747
Maksym-Holovachuk <mgolovachuk@gmail.com>
2023-02-18 23:19:39 +0200

    set tsconfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from 'components/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,5 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
